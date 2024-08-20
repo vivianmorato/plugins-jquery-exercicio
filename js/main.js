9f7f5e37537550ac7b0cc2d19434b7d92a08949d
Vívian Morato <viviaanmorato@gmail.com>
2024-08-20 20:18:54 -0300

    Alterações no Layout e Correção no funcionamento dos plugins

--- js/main.js
+++ js/main.js
@@ -1,42 +1,46 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
+$(document).ready(function() {
+    $('#carousel').slick({
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 2000,
+
+    })
 })
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
+$('#telefone').mask('(00) 00000-0000'), 
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+$('#cpf').mask('000.000.000-00'),
+
+$('#cep').mask('00000-000'),
 
 $('form').validate({
     rules: {
         nome: {
-            requerido: true
+            required: true
         },
         email: {
-            requerido: true,
+            required: true,
             email: true
         },
         telefone: {
-            requerido: true
+            required: true
         },
         endereco: {
-            requerido: true
+            required: true
         },
         cep: {
-            requerido: true
+            required: true
         },
         cpf: {
-            requerido: true
+            required: true
         },
+    
     },
     submitHandler: function (form) {
         alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
         form.reset();
     },
-    invalidHandler: function (form, validator) {
+    invalidHandler: function (evento, validador) {
         alert("Por favor, preencha os campos para prosseguir com a compra!");
     }
 })
